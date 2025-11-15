bfa5089d2cd3dd5c62e5e3d551372f0b0de22bee
Vanz  Edward Mantes <vanzedwardmantes@gmail.com>
2025-11-15 08:46:35 +0800

    client modified

--- assets/js/loginAuth.js
+++ assets/js/loginAuth.js
@@ -27,6 +27,7 @@ loginForm.addEventListener("submit", async (e) => {
       localStorage.setItem("phone", data.phone);
       localStorage.setItem("email", data.email);
       localStorage.setItem("loginTime", new Date().getTime()); // For session tracking
+   
 
       // redirect to dashboard
       window.location.href = "/dashboard";
@@ -42,27 +43,4 @@ loginForm.addEventListener("submit", async (e) => {
     loginAlert.classList.add("alert-danger");
     loginAlert.textContent = "Something went wrong. Please try again.";
   }
-});
-document.addEventListener("DOMContentLoaded", function () {
-  const togglePassword = document.querySelector("#togglePassword");
-  const passwordInput = document.querySelector("#loginPassword");
-  const icon = togglePassword.querySelector("i");
-
-  togglePassword.addEventListener("click", function () {
-    // Toggle the type attribute
-    const type =
-      passwordInput.getAttribute("type") === "password" ? "text" : "password";
-    passwordInput.setAttribute("type", type);
-
-    // Toggle the icon
-    if (type === "password") {
-      // If type is password, show the 'eye-slash' icon
-      icon.classList.remove("bi-eye");
-      icon.classList.add("bi-eye-slash");
-    } else {
-      // If type is text, show the 'eye' icon
-      icon.classList.remove("bi-eye-slash");
-      icon.classList.add("bi-eye");
-    }
-  });
 });
